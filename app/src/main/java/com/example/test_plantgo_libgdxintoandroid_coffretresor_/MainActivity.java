62f19ec62536bf2639ba5756928c591d7a0dfffc
Alexandre Zanni <zanni.ad.dev.web@gmail.com>
2021-02-21 11:39:31 +0100

    Correction problèmes d'affichage

--- app/src/main/java/com/example/test_plantgo_libgdxintoandroid_coffretresor_/MainActivity.java
+++ app/src/main/java/com/example/test_plantgo_libgdxintoandroid_coffretresor_/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public Integer timeCountInMilliSeconds = 5000;
-    public static boolean timerOn = true;
+    public static boolean jeuPasEncoreGagne = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void remplirBarre(View view) {
         ProgressBar pb = findViewById(R.id.progressBar);
-
-        new Thread(() -> {
-            try {
-                Thread.sleep(500); // Faire l'action désirée après 5 secondes
-                pb.incrementProgressBy(12); // Remplir la barre de progression
-            }
-            catch(InterruptedException ex){
-                ex.printStackTrace();
-            }
-        }).start();
+        pb.incrementProgressBy(12); // Remplir la barre de progression
     }
 
     public void viderBarre(View view) {
@@ -67,43 +58,26 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTick(long millisUntilFinished) {
-                //Log.e("seconds remaining : ", "seconds remaining : " + millisUntilFinished / 1000);
+                Log.e("seconds remaining : ", "seconds remaining : " + millisUntilFinished / 1000);
                 Integer tempsRestant = Math.toIntExact(millisUntilFinished / 1000);
-                if (timerOn == true) { // Quand le timer est actif
-                    pb.incrementProgressBy(-1);
+
+                    pb.incrementProgressBy(-1); // Quand le timer est actif
+
+                if ((millisUntilFinished / 1000) > 0 && pb.getProgress() < 99 && jeuPasEncoreGagne == true) { // Quand le timer est actif
+                    TextView tv1 = findViewById(R.id.textView);
                     ImageView imgv1 = findViewById(R.id.gifShovel);
                     imgv1.setVisibility(View.VISIBLE);
                     TextView tv2 = findViewById(R.id.chrono);
                     tv2.setVisibility(View.VISIBLE);
                     tv2.setText(String.valueOf(tempsRestant));
                     pb.setVisibility(View.VISIBLE);
-                }
-
-                if ((millisUntilFinished / 1000) == 0 && pb.getProgress() < 100) { // Quand le timer est terminé
-                    Button bt2 = findViewById(R.id.buttonRemplir);
-                    bt2.setVisibility(View.INVISIBLE);
-                    ImageView imgv1 = findViewById(R.id.gifShovel);
-                    imgv1.setVisibility(View.INVISIBLE);
-                    ImageView imgv3 = findViewById(R.id.idle);
-                    imgv3.setVisibility(View.VISIBLE);
+                    tv1.setText("Appuyez le plus vite possible avant la fin du temps imparti !!!");
                     Button bt1 = findViewById(R.id.buttonVider);
-                    bt1.setVisibility(View.VISIBLE);
-                    TextView tv1 = findViewById(R.id.textView);
-                    tv1.setText("Relancez le chronomètre");
-                    TextView tv2 = findViewById(R.id.chrono);
-                    tv2.setVisibility(View.INVISIBLE);
-                    pb.setVisibility(View.INVISIBLE);
-                }
-
-               if (pb.getProgress() < 100 && (millisUntilFinished / 1000) != 0) { // Quand le timer est actif
-                   TextView tv1 = findViewById(R.id.textView);
-                   tv1.setText("Appuyez le plus vite possible avant la fin du temps imparti !!!");
-                   Button bt1 = findViewById(R.id.buttonVider);
-                   bt1.setVisibility(View.INVISIBLE);
-                   ImageView imgv4 = findViewById(R.id.closedChest);
-                   imgv4.setVisibility(View.INVISIBLE);
-                   ImageView imgv3 = findViewById(R.id.idle);
-                   imgv3.setVisibility(View.INVISIBLE);
+                    bt1.setVisibility(View.INVISIBLE);
+                    ImageView imgv4 = findViewById(R.id.closedChest);
+                    imgv4.setVisibility(View.INVISIBLE);
+                    ImageView imgv3 = findViewById(R.id.idle);
+                    imgv3.setVisibility(View.INVISIBLE);
                 }
                 if (pb.getProgress() >= 99) { // Quand le joueur a gagné (barre remplie)
                     Button bt1 = findViewById(R.id.buttonVider);
@@ -111,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                     Button bt3 = findViewById(R.id.buttonSuite);
                     TextView tv1 = findViewById(R.id.textView);
                     tv1.setText("C'est gagné !!!");
-                    timerOn = false;
+                    jeuPasEncoreGagne = false; // Signale que le joueur a gagné
                     bt1.setVisibility(View.INVISIBLE);
                     bt2.setVisibility(View.INVISIBLE);
                     bt3.setVisibility(View.VISIBLE);
@@ -127,13 +101,28 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
+                if (pb.getProgress() < 99 && jeuPasEncoreGagne == true) { // Quand le timer est terminé
+                    Button bt2 = findViewById(R.id.buttonRemplir); 
+                    bt2.setVisibility(View.INVISIBLE);
+                    ImageView imgv1 = findViewById(R.id.gifShovel);
+                    imgv1.setVisibility(View.INVISIBLE);
+                    ImageView imgv3 = findViewById(R.id.idle);
+                    imgv3.setVisibility(View.VISIBLE);
+                    Button bt1 = findViewById(R.id.buttonVider);
+                    bt1.setVisibility(View.VISIBLE);
+                    TextView tv1 = findViewById(R.id.textView);
+                    tv1.setText("Relancez le chronomètre");
+                    TextView tv2 = findViewById(R.id.chrono);
+                    tv2.setVisibility(View.INVISIBLE);
+                    pb.setVisibility(View.INVISIBLE);
+                }
                 setProgressBarValues(); // call to initialize the progress bar values
             }
         }.start();
         countDownTimer.start();
 
 
-        if (timerOn == false) { // Quand le joueur a gagné (barre remplie), on arrêter le timer (et donc de vider la barre)
+        if (jeuPasEncoreGagne == false) { // Quand le joueur a gagné (barre remplie), on arrêter le timer (et donc de vider la barre)
             countDownTimer.cancel();
         }
     }
